fd9cc0faac56a8a9f061b6a73b1a4a9eed2bd8c4
PedroShinon <jp.101zeta@gmail.com>
2023-08-10 16:33:06 -0300

    pre-test-deploy tsc commit

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
+const cors_1 = __importDefault(require("cors"));
 const users_1 = __importDefault(require("./routes/users"));
 const notifications_routes_1 = __importDefault(require("./routes/notifications-routes"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 //port
 const port = process.env.PORT || 8000;
 //listen
