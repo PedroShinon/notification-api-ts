83a46f8def4d4eb9509596c11cbfbab69a69ac7d
PedroShinon <jp.101zeta@gmail.com>
2023-08-10 17:07:38 -0300

    pre-test-deploy tsc commit

--- dist/routes/notifications-routes.js
+++ dist/routes/notifications-routes.js
@@ -15,20 +15,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const web_push_1 = __importDefault(require("web-push"));
 const zod_1 = require("zod");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 //import { PrismaClient } from "@prisma/client";
 //const prisma = new PrismaClient()
 const router = express_1.default.Router();
 //WebPush.generateVAPIDKeys();
+const urlfront = process.env.FRONTURL || 'https://microcell-8sjaqk37d-yraffic02.vercel.app/';
 const publicKey = 'BNDcqDNFUP14-910AzOdUT3DzgdTez2eiWvrkJKFRtPIU2i0PQgtwBtXFDvsfOFXa1L3Fd548gLkh-yMDRkI2ps';
 const privateKey = 'zADv389I3dWTex_I-FJwRkGHNRpIXC99bsiLT2BBbqI';
-web_push_1.default.setVapidDetails('https://microcell-8sjaqk37d-yraffic02.vercel.app/', publicKey, privateKey);
+web_push_1.default.setVapidDetails(urlfront, publicKey, privateKey);
 router.get("/push/public_key", function (req, res) {
     return res.status(200).json(publicKey);
 });
 router.post("/push/register", function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        //console.log(req.body)
-        return res.status(201).json();
+        console.log(req.body);
+        return res.status(200).json();
     });
 });
 router.post("/push/send", function (req, res) {
