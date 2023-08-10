d9533f5e9a198f3a1ccd484c5b0c1738766f84d8
PedroShinon <jp.101zeta@gmail.com>
2023-08-10 17:06:04 -0300

    pre-test-deploy tsc commit

--- src/routes/notifications-routes.ts
+++ src/routes/notifications-routes.ts
@@ -1,17 +1,19 @@
 import express from "express"
 import WebPush from "web-push"
 import {z} from "zod"
+import {config} from "dotenv"
+config()
 //import { PrismaClient } from "@prisma/client";
 
 //const prisma = new PrismaClient()
 const router = express.Router();
 
 //WebPush.generateVAPIDKeys();
-
+const urlfront = process.env.FRONTURL || 'https://microcell-8sjaqk37d-yraffic02.vercel.app/'
 const publicKey = 'BNDcqDNFUP14-910AzOdUT3DzgdTez2eiWvrkJKFRtPIU2i0PQgtwBtXFDvsfOFXa1L3Fd548gLkh-yMDRkI2ps'
 const privateKey = 'zADv389I3dWTex_I-FJwRkGHNRpIXC99bsiLT2BBbqI'
 
-WebPush.setVapidDetails('https://microcell-8sjaqk37d-yraffic02.vercel.app/', publicKey, privateKey)
+WebPush.setVapidDetails(urlfront, publicKey, privateKey)
 
 
 
@@ -21,9 +23,9 @@ router.get("/push/public_key", function(req, res) {
 
 router.post("/push/register", async function(req, res) {
 
-    //console.log(req.body)
+    console.log(req.body)
 
-    return res.status(201).json()
+    return res.status(200).json()
 
 
 })
